ebae2a087a80766c19c94524fb79a32ab228bd38
Mona Lisa <muzikalrate@gmail.com>
2020-09-04 23:24:18 -0400

    updated to have various important files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lambdata-faren
+
+A first attempt at creating a password generator. This is currently able to creating a password with lower case, upper case, and numbers. Special characters are only to be included with 2 of the options previously mentioned. Currently attempting to fix this problem.
---
+++ setup.py
@@ -0,0 +1,20 @@
+# setup.py file
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Password Generator", # the name that you will install via pip
+    version="1.0",
+    author="Raymond Huang",
+    author_email="dravech.1@gmail.com",
+    description="First attempt at a python package that involves generation of passwords",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/alf-faren/Lambdata-faren",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
